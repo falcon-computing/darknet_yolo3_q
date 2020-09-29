645571209e6ba3d23073df8e7c7f92aa93cb8a99
Reza <reza@Rezas-MacBook-Pro.local>
2020-09-28 17:30:54 -0700

    ready

--- examples/detector.c
+++ examples/detector.c
@@ -610,7 +610,7 @@ void test_detector(char *datacfg, char *cfgfile, char *weightfile, char *filenam
         float *X = sized.data;
         float *X_q = synth_img; 
         time=what_time_is_it_now();
-        network_predict(net, X_q);
+        network_predict(net, X);
         printf("%s: Predicted in %f seconds.\n", input, what_time_is_it_now()-time);
         int nboxes = 0;
         detection *dets = get_network_boxes(net, im.w, im.h, thresh, hier_thresh, 0, 1, &nboxes);
